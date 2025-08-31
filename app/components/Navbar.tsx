b9d4bc5a41662163956ca15d2e4686bca77171cd
Jeeva Krishna V <88268605+Jeevakrishna@users.noreply.github.com>
2025-09-01 02:23:19 +0530

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -62,7 +62,7 @@ function DesktopNav() {
       <div className="left-4 w-[104px]">
         <Link href="/" aria-label="Home">
           <Image
-            src="/1JK.svg"
+            src="/Jeeva10.png"
             alt="JeevaKrishna's Logo"
             width={16}
             height={16}
